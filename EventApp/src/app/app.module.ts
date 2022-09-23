a3b2ff5a6a615f6932e9026a41b2a7073ccfd4cb
allisheik <69035155+allisheik@users.noreply.github.com>
2022-09-23 17:38:49 +0530

    Add files via upload

---
+++ EventApp/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+ 
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    UserComponent,
+    LoginComponent,
+    
+  
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
